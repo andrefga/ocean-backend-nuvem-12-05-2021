c62caf3c36951f62533f9083280557a1f52296b9
andrefga <andrefga@hotmail.com>
2021-05-12 12:26:13 -0300

    Configuração para endppint MongoDB e utilização do MongoDB Atlas

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 (async () => {
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://andre:w5ZHh2DAGPdni63u@cluster0.7iacu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const dbName = 'ocean_bancodados_11_05_2021';
+//const url = 'mongodb://localhost:27017';
+// usuario andre
+// Senha do MongoDB na web w5ZHh2DAGPdni63u
+const dbName = 'ocean_db';
 
 console.info('Conectando ao banco de dados...');
 
@@ -37,49 +40,52 @@ app.get('/mensagens', async (req, res) => {
 });
 
 // GET: READ SINGLE (exibir apenas um registro)
-app.get('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.get('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  const mensagem = mensagens[id];
+  const mensagem = await mensagensCollection.findOne({ _id: ObjectId(id) });
 
   if (!mensagem) {
     res.send('Mensagem não encontrada.');
+
+    return;
   }
 
   res.send(mensagem);
 });
 
+
 // POST: CREATE (criar um registro)
-app.post('/mensagens', (req, res) => {
-  const mensagem = req.body.mensagem;
+app.post('/mensagens', async (req, res) => {
+  const mensagem = req.body;
 
-  mensagens.push(mensagem);
+  await mensagensCollection.insertOne(mensagem);
 
-  const id = mensagens.length;
-
-  res.send(`Mensagem '${id}' criada com sucesso.`);
+  res.send(mensagem);
 });
 
 // PUT: UPDATE (editar um registro)
-app.put('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.put('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  const mensagem = req.body.mensagem;
+  const mensagem = req.body;
 
-  mensagens[id] = mensagem;
+  await mensagensCollection.updateOne(
+    { _id: ObjectId(id) }, { $set: mensagem}
+  );
 
   res.send('Mensagem atualizada com sucesso.');
 });
 
 // DELETE: DELETE (remover um registro)
-app.delete('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.delete('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  delete mensagens[id];
+  await mensagensCollection.deleteOne({ _id: ObjectId(id) });
 
   res.send('Mensagem removida com sucesso.');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 })();
